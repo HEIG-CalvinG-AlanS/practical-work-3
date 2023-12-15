ec266797995a083b5b593f01d993be5a383a2a64
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2023-12-15 15:43:42 +0100

    Refactor BusControllor to send request to server and wait for response

--- src/main/java/pw3/clients/BusController.java
+++ src/main/java/pw3/clients/BusController.java
@@ -1,103 +1,72 @@
 package pw3.clients;
 
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Callable;
 
 @Command(
         name = "bus-controller",
         description = "Start an UDP unicast client"
 )
-public class BusController {
+public class BusController implements Callable<Integer> {
+    @CommandLine.Option(
+            names = {"-H", "--host"},
+            description = "Host to connect to.",
+            scope = CommandLine.ScopeType.INHERIT,
+            required = true
+    )
+    protected String host;
 
-    private static final int PORT = 1234;
-    private static final String HOST = "localhost";
-
-    private void handleEmitterUnicast() {
-        try (DatagramSocket socket = new DatagramSocket()) {
-            String myself = InetAddress.getLocalHost().getHostAddress() + ":" + PORT;
-            System.out.println("Unicast emitter started (" + myself + ")");
-
-            InetAddress serverAddress = InetAddress.getByName(HOST);
-            ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
-
-            scheduler.scheduleAtFixedRate(() -> {
-                try {
-                    String message = "Hello, from unicast bus controller emitter! (" + myself + ")";
-
-                    byte[] payload = message.getBytes(StandardCharsets.UTF_8);
-
-                    DatagramPacket datagram = new DatagramPacket(
-                            payload,
-                            payload.length,
-                            serverAddress,
-                            PORT
-                    );
-
-                    socket.send(datagram);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }, delay, frequency, TimeUnit.MILLISECONDS);
-
-            // Keep the program running for a while
-            scheduler.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void handleReceiverUnicast() {
-// Logique pour gérer la réception Unicast
-        try (DatagramSocket socket = new DatagramSocket(PORT)) {
-            String myself = InetAddress.getLocalHost().getHostAddress() + ":" + PORT;
-            System.out.println("Unicast receiver started (" + myself + ")");
-
-            byte[] receiveData = new byte[1024];
-
-            while (true) {
-                DatagramPacket packet = new DatagramPacket(
-                        receiveData,
-                        receiveData.length
-                );
-
-                socket.receive(packet);
-
-                String message = new String(
-                        packet.getData(),
-                        packet.getOffset(),
-                        packet.getLength(),
-                        StandardCharsets.UTF_8
-                );
-
-                System.out.println("Unicast receiver (" + myself + ") received message: " + message);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    @CommandLine.Option(
+            names = {"-p", "--port"},
+            description = "Port to use (default: 1234).",
+            defaultValue = "1234",
+            scope = CommandLine.ScopeType.INHERIT
+    )
+    protected int port;
 
     @Override
     public Integer call() {
-        Thread unicastReceiverThread = new Thread(this::handleReceiverUnicast);
-        Thread unicastEmitterThread = new Thread(this::handleEmitterUnicast);
 
-        unicastReceiverThread.start();
-        unicastEmitterThread.start();
+        try (DatagramSocket socket = new DatagramSocket()) {
+            InetAddress serverAddress = InetAddress.getByName(host);
 
-        try {
-            unicastReceiverThread.join();
-            unicastEmitterThread.join();
-        } catch (InterruptedException e) {
+            String message = "GET_BUSES";
+
+            byte[] payload = message.getBytes(StandardCharsets.UTF_8);
+
+            DatagramPacket packet = new DatagramPacket(
+                    payload,
+                    payload.length,
+                    serverAddress,
+                    port
+            );
+
+            socket.send(packet);
+
+            // Receive response from server
+            byte[] buffer = new byte[1024];
+
+            packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+
+            String response = new String(
+                    packet.getData(),
+                    packet.getOffset(),
+                    packet.getLength(),
+                    StandardCharsets.UTF_8
+            );
+
+            System.out.println(response);
+        } catch (Exception e) {
             e.printStackTrace();
             return 1;
         }
+
         return 0;
     }
 }
