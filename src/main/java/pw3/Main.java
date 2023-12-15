13d3eccdcae8e8c0fa34e807b0e08030b142a001
Ludovic Delafontaine <ludovic.delafontaine@gmail.com>
2023-12-15 14:33:28 +0100

    Remove unnecessary code in Main to execute the application with picocli

--- src/main/java/pw3/Main.java
+++ src/main/java/pw3/Main.java
@@ -21,30 +21,7 @@ import static java.lang.Thread.sleep;
 )
 
 public class Main {
-    public static void main(String... args) throws InterruptedException {
-
-        BusLineEmitter bl1 = new BusLineEmitter(101);
-        BusLineEmitter bl2 = new BusLineEmitter(102);
-
-        bl1.addingTrajectoryStop("BusStop 1", 0);
-        bl1.addingTrajectoryStop("BusStop 2", 1000);
-        bl1.addingTrajectoryStop("BusStop 3", 4000);
-        bl1.addingTrajectoryStop("BusStop 5", 3000);
-
-
-        bl2.addingTrajectoryStop("BusStop 1", 3);
-
-        bl1.insertingTrajectoryStop("BusStop 4", 7000, "BusStop 3");
-
-
-        bl1.addingBusToLine(new BusLineEmitter.Bus(3,10));
-        bl1.addingBusToLine(new BusLineEmitter.Bus(7,10));
-
-        //les starts en threads
-        bl1.getBus(0).start();
-        sleep(10000);
-        bl1.getBus(1).start();
-
+    public static void main(String... args) {
         // Source: https://stackoverflow.com/a/11159435
         String commandName = new java.io.File(
                 Main.class.getProtectionDomain()
